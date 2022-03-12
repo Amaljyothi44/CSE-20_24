a4ecab47d3ce8cbece1f01d82574e4687d7c6d0a
VishnuSanal <t.v.s10123@gmail.com>
2022-03-12 16:52:38 +0530

    update search

--- Vishnu_S3_CSE/DS/11. Search.c
+++ Vishnu_S3_CSE/DS/11. Search.c
@@ -1,21 +1,59 @@
 #include <stdio.h>
 
-void swap(int *a, int *b) {
-  int temp = *a;
-  *a = *b;
-  *b = temp;
+int linearSearch(int[], int, int);
+int binarySearch(int[], int, int);
+
+int *bubbleSort(int[], int);
+
+void swap(int *, int *);
+void printArray(char[], int *, int);
+
+int main() {
+
+  // int arr[] = {-2, 45, 0, 11, -9};
+  // int n = sizeof(arr) / sizeof(arr[0]);
+  // int key = 45;
+
+  int n, key;
+
+  printf("Enter the size of the array: ");
+  scanf("%d", &n);
+
+  int arr[n];
+
+  for (int i = 0; i < n; i++) {
+    printf("Enter the %dth element: ", i);
+    scanf("%d", &arr[i]);
+  }
+
+  printf("Enter the value to be searched for: ");
+  scanf("%d", &key);
+
+  printArray("Original array:", arr, n);
+
+  printf("\n");
+
+  printf("\nLinear Search: Index: %d\n", linearSearch(arr, n, key));
+
+  printf("\nSorting the array to perform binary search...\n");
+
+  *arr = *bubbleSort(arr, n);
+
+  printArray("Sorted array:", arr, n);
+
+  printf("\n\nBinary Search: Index: %d", binarySearch(arr, n, key));
+
+  printf("\n\n");
+
+  return 0;
 }
 
 int *bubbleSort(int arr[], int n) {
 
-  for (int i = 0; i < n - 1; i++) {
-    for (int j = 0; j < n - i - 1; j++) {
-
-      if (arr[j] > arr[j + 1]) {
+  for (int i = 0; i < n - 1; i++)
+    for (int j = 0; j < n - i - 1; j++)
+      if (arr[j] > arr[j + 1])
         swap(&arr[j], &arr[j + 1]);
-      }
-    }
-  }
 
   return arr;
 }
@@ -37,15 +75,21 @@ int binarySearch(int arr[], int n, int key) {
 
     int i = (left + right) / 2;
 
-    if (arr[i] == key) {
+    if (arr[i] == key)
       return i;
-    } else if (key > arr[i])
+    else if (key > arr[i])
       left = i + 1;
     else
       right = i - 1;
   }
 
   return -1;
+}
+
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
 }
 
 void printArray(char s[], int *arr, int n) {
@@ -56,40 +100,3 @@ void printArray(char s[], int *arr, int n) {
     printf("%d\t", arr[i]);
 }
 
-int main() {
-
-  int arr[] = {-2, 45, 0, 11, -9};
-  int n = sizeof(arr) / sizeof(arr[0]);
-  int key = 45;
-
-  /*int n;
-
-  printf("Enter the size of the array: ");
-  scanf("%d", &n);
-
-  int arr[n];
-
-  for (int i = 0; i < n; i++) {
-    printf("Enter the %dth element: ", i);
-    scanf("%d", &arr[i]);
-  }
-  */
-
-  printArray("Original array:", arr, n);
-
-  printf("\n");
-
-  printf("\nLinear Search: Index: %d\n", linearSearch(arr, n, key));
-
-  printf("\nSorting the array to perform binary search...\n");
-
-  *arr = *bubbleSort(arr, n);
-
-  printArray("Sorted array:", arr, n);
-
-  printf("\n\nBinary Search: Index: %d", binarySearch(arr, n, key));
-
-  printf("\n\n");
-
-  return 0;
-}
