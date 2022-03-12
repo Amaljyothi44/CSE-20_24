407b9c883411fa5034e47cb9470dabeb34f02cf1
VishnuSanal <t.v.s10123@gmail.com>
2022-03-12 15:46:31 +0530

    update polynomial addition

--- Vishnu_S3_CSE/DS/7. PolynomialAddition.c
+++ Vishnu_S3_CSE/DS/7. PolynomialAddition.c
@@ -1,147 +1,153 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 struct Node {
-        int coefficient;
-        int exponent;
-        struct Node *link;
-    };
+  int coefficient;
+  int exponent;
+  struct Node *link;
+};
 
-struct Node* insertEnd(struct Node* HEAD, struct Node* newNode) {
+struct Node *read();
+struct Node *add(struct Node *, struct Node *);
+void display(struct Node *);
+struct Node *insertEnd(struct Node *, struct Node *);
 
-    if(HEAD == NULL)
-        return newNode;
+int main() {
 
-    struct Node* ptr = HEAD;
+  struct Node *first, *second;
 
-    while(ptr->link != NULL)
-        ptr = ptr->link;
+  printf("\nEnter the first polynomial: \n");
+  first = read();
 
-    ptr->link = newNode;
-    newNode->link = NULL;
+  printf("\nThe first polynomial is: ");
+  display(first);
 
-    return HEAD;
+  printf("\nEnter the second polynomial: \n");
+  second = read();
+
+  printf("\nThe second polynomial is: ");
+  display(second);
+
+  printf("\nThe sum of two polynomials are: ");
+  display(add(first, second));
+
+  return 0;
 }
 
-void display(struct Node* polynomial){
+struct Node *insertEnd(struct Node *HEAD, struct Node *newNode) {
 
-    struct Node* ptr;
+  if (HEAD == NULL)
+    return newNode;
 
-    ptr = polynomial;
+  struct Node *ptr = HEAD;
 
-    while(ptr != NULL) {
-        printf("%dX^%d ", ptr->coefficient, ptr->exponent);
-        ptr = ptr->link;
+  while (ptr->link != NULL)
+    ptr = ptr->link;
 
-        if(ptr != NULL)
-            printf("+ "); // printf("%c ", ptr->coefficient > 0 ? '+' : '-');
+  ptr->link = newNode;
+  newNode->link = NULL;
+
+  return HEAD;
+}
+
+void display(struct Node *polynomial) {
+
+  struct Node *ptr;
+
+  ptr = polynomial;
+
+  while (ptr != NULL) {
+
+    printf("%dX^%d ", ptr->coefficient, ptr->exponent);
+    ptr = ptr->link;
+
+    if (ptr != NULL)
+      printf("+ "); // printf("%c ", ptr->coefficient > 0 ? '+' : '-');
+  }
+
+  printf("\n");
+}
+
+struct Node *read() {
+
+  struct Node *HEAD = NULL;
+
+  while (1) {
+
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node *));
+
+    printf("Enter the coefficient: ");
+    scanf("%d", &newNode->coefficient);
+
+    printf("Enter the exponent: ");
+    scanf("%d", &newNode->exponent);
+
+    HEAD = insertEnd(HEAD, newNode);
+
+    int choice;
+    printf("Enter 0 to exit and 1 to continue adding more terms: ");
+    scanf("%d", &choice);
+
+    if (choice == 0)
+      return HEAD;
+  }
+}
+
+struct Node *add(struct Node *first, struct Node *second) {
+
+  struct Node *HEAD = NULL;
+
+  while (first != NULL && second != NULL) {
+
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node *));
+
+    if (first->exponent > second->exponent) {
+
+      newNode->exponent = first->exponent;
+      newNode->coefficient = first->coefficient;
+
+      first = first->link;
+
+    } else if (second->exponent > first->exponent) {
+
+      newNode->exponent = second->exponent;
+      newNode->coefficient = second->coefficient;
+
+      second = second->link;
+
+    } else {
+
+      newNode->exponent = first->exponent;
+      newNode->coefficient = first->coefficient + second->coefficient;
+
+      first = first->link;
+      second = second->link;
     }
 
-    printf("\n");
+    HEAD = insertEnd(HEAD, newNode);
+  }
 
-}
+  while (first != NULL || second != NULL) {
 
-struct Node* read() {
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node *));
 
-    struct Node* HEAD = NULL;
+    if (first != NULL) {
 
-    while(1) {
+      newNode->exponent = first->exponent;
+      newNode->coefficient = first->coefficient;
 
-        struct Node* newNode = (struct Node*) malloc(sizeof(struct Node*));
+      first = first->link;
 
-        printf("Enter the coefficient: ");
-        scanf("%d", &newNode->coefficient);
-        printf("Enter the exponent: ");
-        scanf("%d", &newNode->exponent);
+    } else {
 
-        HEAD = insertEnd(HEAD, newNode);
+      newNode->exponent = second->exponent;
+      newNode->coefficient = second->coefficient;
 
-        int choice;
-        printf("Enter 0 to exit and 1 to continue adding more terms: ");
-        scanf("%d", &choice);
-
-        if(choice == 0)
-            return HEAD;
-
+      second = second->link;
     }
 
-}
+    HEAD = insertEnd(HEAD, newNode);
+  }
 
-struct Node* add(struct Node* first, struct Node* second) {
-
-    struct Node* HEAD = NULL;
-
-    while(first != NULL && second != NULL) {
-
-        struct Node* newNode = (struct Node*) malloc(sizeof(struct Node*));
-
-        if(first->exponent > second->exponent) {
-
-            newNode->exponent = first->exponent;
-            newNode->coefficient = first->coefficient;
-            first = first->link;
-
-        } else if(second->exponent > first->exponent) {
-
-            newNode->exponent = second->exponent;
-            newNode->coefficient = second->coefficient;
-            second = second->link;
-
-        }  else {
-
-            newNode->exponent = first->exponent;
-            newNode->coefficient = first->coefficient + second->coefficient;
-            first = first->link;
-            second = second->link;
-
-        }
-
-        HEAD = insertEnd(HEAD, newNode);
-
-    }
-
-    while(first != NULL || second != NULL) {
-
-            struct Node* newNode = (struct Node*) malloc(sizeof(struct Node*));
-
-            if(first !=  NULL) {
-
-                newNode->exponent = first->exponent;
-                newNode->coefficient = first->coefficient;
-                first = first->link;
-
-            } else {
-
-                newNode->exponent = second->exponent;
-                newNode->coefficient = second->coefficient;
-                second = second->link;
-
-            }
-
-            HEAD = insertEnd(HEAD, newNode);
-
-        }
-
-    return HEAD;
-
-}
-
-void main(){
-
-    struct Node *first, *second;
-
-    printf("\nEnter the first polynomial: \n");
-    first = read();
-    printf("\nThe first polynomial is: ");
-    display(first);
-
-    printf("\nEnter the second polynomial: \n");
-    second = read();
-    printf("\nThe second polynomial is: ");
-    display(second);
-
-    printf("\nThe sum of two polynomials are: ");
-    display(add(first, second));
-
+  return HEAD;
 }
