67d7a9ce68ea896c795a0496c5a5f4c9a650042d
VishnuSanal <t.v.s10123@gmail.com>
2022-03-12 16:41:37 +0530

    update sort

--- Vishnu_S3_CSE/DS/10. Sort.c
+++ Vishnu_S3_CSE/DS/10. Sort.c
@@ -1,181 +1,20 @@
 #include <stdio.h>
 
-void swap(int *a, int *b) {
-  int temp = *a;
-  *a = *b;
-  *b = temp;
-}
+int *bubbleSort(int[], int);
+int *insertionSort(int[], int);
+int *selectionSort(int[], int);
 
-int *bubbleSort(int arr[], int n) {
+int *quickSort(int[], int, int);
+int quickSortPartition(int[], int, int);
 
-  for (int i = 0; i < n - 1; i++) {
-    for (int j = 0; j < n - i - 1; j++) {
+int *mergeSort(int[], int, int);
+void mergeSortMerge(int[], int, int, int);
 
-      if (arr[j] > arr[j + 1]) {
-        swap(&arr[j], &arr[j + 1]);
-      }
-    }
-  }
+int *heapSort(int[], int);
+void heapSortHeapify(int[], int, int);
 
-  return arr;
-}
-
-int *insertionSort(int arr[], int n) {
-
-  for (int i = 1; i < n; i++) {
-
-    int key = arr[i], j = i - 1;
-
-    while (arr[j] > key && j >= 0)
-      arr[j + 1] = arr[j--];
-
-    arr[j + 1] = key;
-  }
-
-  return arr;
-}
-
-int *selectionSort(int arr[], int n) {
-
-  for (int i = 0; i < n - 1; i++) {
-
-    int min = i;
-
-    for (int j = i + 1; j < n; j++) {
-
-      if (arr[j] < arr[min])
-        min = j;
-    }
-
-    swap(&arr[i], &arr[min]);
-  }
-
-  return arr;
-}
-
-int quickSortPartition(int arr[], int low, int high) {
-
-  int pivot = arr[high];
-
-  int i = (low - 1);
-
-  for (int j = low; j < high; j++) {
-    if (arr[j] <= pivot) {
-
-      i++;
-
-      swap(&arr[i], &arr[j]);
-    }
-  }
-
-  swap(&arr[i + 1], &arr[high]);
-
-  return (i + 1);
-}
-
-int *quickSort(int arr[], int low, int high) {
-  if (low < high) {
-
-    int p = quickSortPartition(arr, low, high);
-
-    quickSort(arr, low, p - 1);
-
-    quickSort(arr, p + 1, high);
-  }
-
-  return arr;
-}
-
-void mergeSortMerge(int arr[], int p, int q, int r) {
-
-  int n1 = q - p + 1;
-  int n2 = r - q;
-
-  int L[n1], M[n2];
-
-  for (int i = 0; i < n1; i++)
-    L[i] = arr[p + i];
-  for (int j = 0; j < n2; j++)
-    M[j] = arr[q + 1 + j];
-
-  int i, j, k;
-  i = 0;
-  j = 0;
-  k = p;
-
-  while (i < n1 && j < n2) {
-    if (L[i] <= M[j]) {
-      arr[k] = L[i];
-      i++;
-    } else {
-      arr[k] = M[j];
-      j++;
-    }
-    k++;
-  }
-
-  while (i < n1) {
-    arr[k] = L[i];
-    i++;
-    k++;
-  }
-
-  while (j < n2) {
-    arr[k] = M[j];
-    j++;
-    k++;
-  }
-}
-
-int *mergeSort(int arr[], int l, int r) {
-  if (l < r) {
-
-    int m = l + (r - l) / 2;
-
-    mergeSort(arr, l, m);
-    mergeSort(arr, m + 1, r);
-
-    mergeSortMerge(arr, l, m, r);
-  }
-  return arr;
-}
-
-void heapSortHeapify(int arr[], int n, int i) {
-  int largest = i;
-  int left = 2 * i + 1;
-  int right = 2 * i + 2;
-
-  if (left < n && arr[left] > arr[largest])
-    largest = left;
-
-  if (right < n && arr[right] > arr[largest])
-    largest = right;
-
-  if (largest != i) {
-    swap(&arr[i], &arr[largest]);
-    heapSortHeapify(arr, n, largest);
-  }
-}
-
-int *heapSort(int arr[], int n) {
-  for (int i = n / 2 - 1; i >= 0; i--)
-    heapSortHeapify(arr, n, i);
-
-  for (int i = n - 1; i >= 0; i--) {
-    swap(&arr[0], &arr[i]);
-
-    heapSortHeapify(arr, i, 0);
-  }
-  return arr;
-}
-
-void printArray(char s[], int *arr, int n) {
-
-  printf("\n%s\n", s);
-
-  for (int i = 0; i < n; i++)
-    printf("%d\t", arr[i]);
-}
+void swap(int *, int *);
+void printArray(char s[], int *arr, int n);
 
 int main() {
 
@@ -205,4 +44,165 @@ int main() {
   printf("\n");
 
   return 0;
+}
+
+int *bubbleSort(int arr[], int n) {
+
+  for (int i = 0; i < n - 1; i++)
+    for (int j = 0; j < n - i - 1; j++)
+      if (arr[j] > arr[j + 1])
+        swap(&arr[j], &arr[j + 1]);
+
+  return arr;
+}
+
+int *insertionSort(int arr[], int n) {
+
+  for (int i = 1; i < n; i++) {
+
+    int key = arr[i], j = i - 1;
+
+    while (arr[j] > key && j >= 0)
+      arr[j + 1] = arr[j--];
+
+    arr[j + 1] = key;
+  }
+
+  return arr;
+}
+
+int *selectionSort(int arr[], int n) {
+
+  for (int i = 0; i < n - 1; i++) {
+
+    int min = i;
+
+    for (int j = i + 1; j < n; j++)
+      if (arr[j] < arr[min])
+        min = j;
+
+    swap(&arr[i], &arr[min]);
+  }
+
+  return arr;
+}
+
+int *quickSort(int arr[], int low, int high) {
+  if (low < high) {
+
+    int p = quickSortPartition(arr, low, high);
+
+    quickSort(arr, low, p - 1);
+
+    quickSort(arr, p + 1, high);
+  }
+
+  return arr;
+}
+
+int quickSortPartition(int arr[], int low, int high) {
+
+  int pivot = arr[high];
+
+  int i = (low - 1);
+
+  for (int j = low; j < high; j++) {
+    if (arr[j] <= pivot) {
+
+      i++;
+
+      swap(&arr[i], &arr[j]);
+    }
+  }
+
+  swap(&arr[i + 1], &arr[high]);
+
+  return (i + 1);
+}
+
+int *mergeSort(int arr[], int l, int r) {
+  if (l < r) {
+
+    int m = l + (r - l) / 2;
+
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+
+    mergeSortMerge(arr, l, m, r);
+  }
+  return arr;
+}
+
+void mergeSortMerge(int arr[], int p, int q, int r) {
+
+  int n1 = q - p + 1;
+  int n2 = r - q;
+
+  int L[n1], M[n2];
+
+  for (int i = 0; i < n1; i++)
+    L[i] = arr[p + i];
+
+  for (int j = 0; j < n2; j++)
+    M[j] = arr[q + 1 + j];
+
+  int i = 0, j = 0, k = p;
+
+  while (i < n1 && j < n2)
+    if (L[i] <= M[j])
+      arr[k++] = L[i++];
+    else
+      arr[k++] = M[j++];
+
+  while (i < n1)
+    arr[k++] = L[i++];
+
+  while (j < n2)
+    arr[k++] = M[j++];
+}
+
+int *heapSort(int arr[], int n) {
+
+  for (int i = n / 2 - 1; i >= 0; i--)
+    heapSortHeapify(arr, n, i);
+
+  for (int i = n - 1; i >= 0; i--) {
+    swap(&arr[0], &arr[i]);
+
+    heapSortHeapify(arr, i, 0);
+  }
+
+  return arr;
+}
+
+void heapSortHeapify(int arr[], int n, int i) {
+
+  int largest = i;
+  int left = 2 * i + 1;
+  int right = 2 * i + 2;
+
+  if (left < n && arr[left] > arr[largest])
+    largest = left;
+
+  if (right < n && arr[right] > arr[largest])
+    largest = right;
+
+  if (largest != i) {
+    swap(&arr[i], &arr[largest]);
+    heapSortHeapify(arr, n, largest);
+  }
+}
+
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void printArray(char s[], int *arr, int n) {
+
+  printf("\n%s\n", s);
+
+  for (int i = 0; i < n; i++)
+    printf("%d\t", arr[i]);
 }
