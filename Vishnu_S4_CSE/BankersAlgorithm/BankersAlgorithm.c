d7ebe71c6554238627560e968ed15a7898bd5c99
VishnuSanal <t.v.s10123@gmail.com>
2022-07-31 00:31:10 +0530

    update bankers algorithm

--- Vishnu_S4_CSE/BankersAlgorithm/BankersAlgorithm.c
+++ Vishnu_S4_CSE/BankersAlgorithm/BankersAlgorithm.c
@@ -1,88 +1,130 @@
+#include <stdbool.h>
 #include <stdio.h>
+
+bool bankersAlgorithm(int n, int m, int alloc[n][m], int max[n][m],
+                      int need[n][m], int avail[m], bool isResourceRequest) {
+
+  int visited[n], sequence[n], index = 0;
+
+  for (int i = 0; i < n; i++)
+    visited[i] = 0;
+
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      need[i][j] = max[i][j] - alloc[i][j];
+
+  int y = 0;
+
+  for (int k = 0; k < n; k++)
+    for (int i = 0; i < n; i++)
+      if (visited[i] == 0) {
+
+        int flag = 0;
+
+        for (int j = 0; j < m; j++)
+          if (need[i][j] > avail[j]) {
+            flag = 1;
+            break;
+          }
+
+        if (flag == 0) {
+
+          sequence[index++] = i;
+
+          for (int y = 0; y < m; y++)
+            avail[y] += alloc[i][y];
+
+          visited[i] = 1;
+        }
+      }
+
+  int flag = 1;
+
+  for (int i = 0; i < n; i++)
+    if (visited[i] == 0) {
+      flag = 0;
+      printf("System not SAFE");
+
+      if (isResourceRequest)
+        printf(" => New request can't be allocated");
+
+      return false;
+    }
+
+  if (flag == 1) {
+
+    printf("SAFE Sequence: ");
+
+    for (int i = 0; i < n - 1; i++)
+      printf("P%d ->\t", sequence[i]);
+
+    printf("P%d\t", sequence[n - 1]);
+
+    if (isResourceRequest)
+      printf("\n => New request can be allocated");
+  }
+
+  return true;
+}
 
 int main() {
 
-	int n = 0, m = 0;
+  int n = 0, m = 0;
 
-	printf("Enter the number of processes (n): ");
-	scanf("%d", &n);
-	
-	printf("Enter the number of resources (m): ");
-	scanf("%d", &m);
+  printf("Enter the number of processes (n): ");
+  scanf("%d", &n);
 
-	int alloc[n][m], max[n][m], avail[m];
+  printf("Enter the number of resources (m): ");
+  scanf("%d", &m);
 
-	printf("Enter the allocation matrix [n x m]:\n");
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++) {
-			scanf("%d", &alloc[i][j]);
-		}
+  int alloc[n][m], max[n][m], avail[m], need[n][m];
 
-	printf("Enter the maximum matrix [n x m]:\n");
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			scanf("%d", &max[i][j]);
+  printf("Enter the allocation matrix [n x m]:\n");
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      scanf("%d", &alloc[i][j]);
 
-	printf("Enter the avail matrix [1 x m]:\n");
-	for (int i = 0; i < m; i++)
-			scanf("%d", &avail[i]);
+  printf("Enter the maximum matrix [n x m]:\n");
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < m; j++)
+      scanf("%d", &max[i][j]);
 
-	int visited[n], sequence[n], index = 0;
-	
-	for (int i = 0; i < n; i++)
-		visited[i] = 0;
+  printf("Enter the avail matrix [1 x m]:\n");
+  for (int i = 0; i < m; i++)
+    scanf("%d", &avail[i]);
 
-	int need[n][m];
-	
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
-			need[i][j] = max[i][j] - alloc[i][j];
+  bool result = bankersAlgorithm(n, m, alloc, max, need, avail, false);
 
-	int y = 0;
-	
-	for (int k = 0; k < n; k++)
-		for (int i = 0; i < n; i++)
-			if (visited[i] == 0) {
+  if (!result)
+    return 0;
 
-				int flag = 0;
+  int new, request[m];
 
-				for (int j = 0; j < m; j++) {
-					if (need[i][j] > avail[j]){
-						flag = 1;
-						break;
-					}
-				}
+  printf("\n\nPID of the process requesting next: ");
+  scanf("%d", &new);
 
-				if (flag == 0) {
-				
-					sequence[index++] = i;
-					
-					for (int y = 0; y < m; y++)
-						avail[y] += alloc[i][y];
-						
-					visited[i] = 1;
-				}
-			}
+  printf("Enter the need vector of the next request [1 x m]: ");
+  for (int i = 0; i < m; i++)
+    scanf("%d", &request[i]);
 
-	int flag = 1;
-	
-	for(int i = 0; i < n; i++)
-		if(visited[i] == 0) {
-			flag = 0;
-			printf("System not SAFE");
-			break;
-		}
-	
-	if(flag == 1) {
-		printf("SAFE Sequence: ");
+  for (int i = 0; i < m; i++) {
+    if (request[i] > need[new][i]) {
+      printf(
+          "Invalid request: Request > Need => New request can't be allocated");
+      return 0;
+    }
 
-		for (int i = 0; i < n - 1; i++)
-			printf("P%d ->\t", sequence[i]);
+    if (request[i] > avail[i]) {
+      printf(
+          "Invalid request: Request > Avail => New request can't be allocated");
+      return 0;
+    }
+  }
 
-		printf("P%d\t", sequence[n - 1]);
-	}
-	
-	printf("\n\n");
+  bankersAlgorithm(n, m, alloc, max, need, avail, true);
 
-	return 0;
+  printf("\n\n");
+
+  return 0;
 }
+
